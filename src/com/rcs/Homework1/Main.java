34e892142c6d6b6dfb1716e58a48a0e10f65d71a
Artūrs Lobanovs <lobanovs.arturs@gmail.com>
2021-02-27 07:03:03 +0200

    Homework 1

--- src/com/rcs/Homework1/Main.java
+++ src/com/rcs/Homework1/Main.java
@@ -1,8 +1,18 @@
 package com.rcs.Homework1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Sveiki!");
+        System.out.println("šajā programmā Jūs ievadīsiet Fārenhaita grādus " +
+                "un tie būs pārveidoti uz Celsija grādiem!");
+        System.out.print("Lūdzu, ievadiet Fārenhaita grādus: ");
+        Scanner scan = new Scanner(System.in);
+        Double f = scan.nextDouble();
+        scan.close();
+        Double c = 5 * (f - 32) / 9;
+        System.out.println(f + " Fārenhaita grādi ir " + c + " Celsija grādi");
     }
 }
